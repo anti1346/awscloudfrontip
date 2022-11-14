938cda269410b21b0c43c62e8406113f0e539b52
Aby <anti1346@gmail.com>
2022-11-14 12:40:50 +0900

    no message

--- cfipfordnscreate.py
+++ cfipfordnscreate.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-import os
+import os, sys
 import requests
 import ipaddress
+from pathlib import Path
+import filecmp
 
 ip_ranges = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json').json()['prefixes']
 cloudfront_ips = [item['ip_prefix'] for item in ip_ranges if (item["service"] == "CLOUDFRONT" and item["region"] == "ap-northeast-2")]
@@ -34,10 +36,21 @@ for a in iplist:
         if a == b:
             print(a)
 
-# os.system('copy zonefile sangchul.kr.zone')
-# os.system('copy sangchul.kr.zone sangchul.kr.zone_$(date +"%Y%m%d-%H%M%S")')
-os.system('copy sangchul.kr.zone sangchul.kr.zone.bk')
+### zonefile 템플릿 및 백업
+dnszonefile = 'sangchul.kr.zone'
+path = Path(dnszonefile)
+if path.is_file():
+    print(f'The file {dnszonefile} exists')
+    os.system('copy sangchul.kr.zone sangchul.kr.zone.bk')
+else:
+    print(f'The file {dnszonefile} does not exist')
+    os.system('copy zonefile sangchul.kr.zone')
 
+# os.system('copy sangchul.kr.zone sangchul.kr.zone.bk')
+
+
+### sangchul.kr zonefile 생성
+os.system('copy zonefile sangchul.kr.zone')
 dnszonefile = open('sangchul.kr.zone', 'a')
 for a in iplist:
     for b in cloudfrontiplist:
@@ -46,3 +59,14 @@ for a in iplist:
             recode = ('sangchul.kr.\t\t\tIN\tA\t%s\n' %a)
             dnszonefile.write(recode)
 dnszonefile.close()
+
+### zonefile 비교, 원복
+file1 = "sangchul.kr.zone"
+file2 = "sangchul.kr.zone.bk"
+def compare_files(file1,file2):
+    compare = filecmp.cmp(file1,file2)
+    if compare == True:
+        print("The files are the same.")
+    else:
+        print("The files are different.")
+compare_files(file1,file2)
