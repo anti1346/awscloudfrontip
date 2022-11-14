96cd18a0fdc746da65c80813712706ee3e074dd3
Aby <anti1346@gmail.com>
2022-11-14 11:00:55 +0900

    no message

--- cfipfordnscreate.py
+++ cfipfordnscreate.py
@@ -6,19 +6,19 @@ import ipaddress
 ip_ranges = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json').json()['prefixes']
 cloudfront_ips = [item['ip_prefix'] for item in ip_ranges if (item["service"] == "CLOUDFRONT" and item["region"] == "ap-northeast-2")]
 
-### 클라우드프론트 아이피 대역대
-cloudfront_ipaddress=[]
+### 클라우드프론트 서브넷
+cloudfront_seoul_subnet=[]
 for ip in cloudfront_ips:
-    cloudfront_ipaddress.append(ip)
+    cloudfront_seoul_subnet.append(ip)
 
 ### 클라우드프론트 아이피 리스트를 리스트(list)에 저장
-cloudfrontlist=[]
-for ip in cloudfront_ipaddress:
+cloudfrontiplist=[]
+for ip in cloudfront_seoul_subnet:
     net4 = ipaddress.ip_network(str(ip))
     print(net4.hosts)
     for x in net4.hosts():
-        cloudfrontlist.append(format(ipaddress.IPv4Address(x)))
-# print(cloudfrontlist)
+        cloudfrontiplist.append(format(ipaddress.IPv4Address(x)))
+# print(cloudfrontiplist)
 
 ### iplist.txt 파일을 읽어 리스트(list)에 저장
 iplist=[]
@@ -30,48 +30,19 @@ with open('iplist.txt', 'r') as filehandle:
 
 ### iplist.txt의 아이피 리스트와 클라우드프론트 아이피 리스트 비교
 for a in iplist:
-    for b in cloudfrontlist:
+    for b in cloudfrontiplist:
         if a == b:
             print(a)
 
-os.system('copy zonefile sangchul.kr.zone')
+# os.system('copy zonefile sangchul.kr.zone')
+# os.system('copy sangchul.kr.zone sangchul.kr.zone_$(date +"%Y%m%d-%H%M%S")')
+os.system('copy sangchul.kr.zone sangchul.kr.zone.bk')
 
 dnszonefile = open('sangchul.kr.zone', 'a')
 for a in iplist:
-    for b in cloudfrontlist:
+    for b in cloudfrontiplist:
         if a == b:
             # print(a)
             recode = ('sangchul.kr.\t\t\tIN\tA\t%s\n' %a)
             dnszonefile.write(recode)
 dnszonefile.close()
-
-# file1 = open('iplist.txt', 'r')
-# Lines = file1.readlines()
-# count = 0
-# for line in Lines:
-#     count += 1
-#     print(line)
-
-
-
-# for ip in cloudfront_ipaddress:
-#     net4 = ipaddress.ip_network(str(ip))
-#     for x in net4.hosts():
-#         count = 0
-#         for line in Lines:
-#             count += 1
-#             if [line == x]:
-#                 print(line)
-        
-# cloudfrontip = os.system("cat cloudfrontiplist.txt")
-# dnsip = os.system("cat iplist.txt")
-
-
-
-# f = open("cloudfrontiplist.txt", 'r')
-# line = f.readline()
-# print(line)
-# for ip in line:
-#     if ip == dnsip:
-#         print("==")
-#         print(ip)
